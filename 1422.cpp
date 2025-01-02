2cebf1e3f85b5e254641103e530a187e0fa0f6cc
coleutk <colepricecollege@gmail.com>
2025-01-02 14:31:12 -0600

    Maximum Score After Splitting a String

---
+++ 1422.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxScore(string s) {
+        int n = s.size();
+        vector<int> postfixSum(n, 0); 
+
+        // Create a postfix sum (starting from the back) to know how
+        // many 1s come after a certain index in s. This way, as you
+        // iterate forward through s, you can keep track of 0s and 
+        // calculate a sum of 0s and 1s at any point
+        int oneCnt = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            if(s[i] == '1') {
+                onesCnt++; 
+            } 
+            
+            postfixSum[i] = onesCnt; 
+        } 
+
+        int zeroCnt = 0;
+        int mx = INT_MIN;
+        for(int i = 0; i < s.size() - 1; i++) {
+            if(s[i] == '0') {
+                zeroCnt++;
+            } 
+        
+            mx = max(mx, zeroCnt + postfixSum[i + 1]);
+        }
+        
+        return mx;
+    }
+};

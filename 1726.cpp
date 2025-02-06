454889e16c2445bd8255f44323334793486fc35c
coleutk <colepricecollege@gmail.com>
2025-02-06 11:16:19 -0500

    Tuple with Same Product***

--- 1726.cpp
+++ 1726.cpp
@@ -14,7 +14,7 @@ public:
         for(const auto &pair : productFreq) {
             // 4 unique products makes 6 pairs
             // 3 unique products makes 3 pairs
-            // 4 unique products * 3 unique products = total amount of pairs at 4
+            // (4 unique products * 3 unique products) / 2 = total amount of pairs at 4
             // then multiply this number of pairs by 8 to see how many tuples it generates
             int pairs = pair.second * (pair.second - 1) / 2;
             res += pairs * 8;

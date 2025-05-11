510749c3d2b11f281e92b7555aa7973c2240f912
coleutk <colepricecollege@gmail.com>
2025-05-11 12:19:21 -0400

    Three Consecutive Odds

---
+++ 1550.go
@@ -0,0 +1,7 @@
+func threeConsecutiveOdds(arr []int) bool {
+    if len(arr) < 3 { return false }
+    for i := 0; i < len(arr) - 2; i++ {
+        if arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1 { return true }
+    }
+    return false
+}
